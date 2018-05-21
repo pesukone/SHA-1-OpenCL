ca775e763b4d754b44d69e06b58df8f8f235d442
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-05-21 22:42:53 +0300

    finally got it working

---
+++ sha1.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+#include <CL/cl.h>
+
+#define MAX_SOURCE_SIZE (0x100000)
+
+int main(int argc, char** argv) {
+	cl_platform_id* platforms = malloc(2 * sizeof(cl_platform_id));
+	cl_uint ret_num_platforms;
+	cl_device_id* devices = malloc(2 * sizeof(cl_device_id));
+	cl_uint ret_num_devices;
+	cl_context context = NULL;
+	cl_command_queue queue = NULL;
+	cl_program program = NULL;
+	cl_kernel kernel = NULL;
+	cl_int ret;
+
+	FILE* fp = fopen("./sha1.cl", "r");
+	if (!fp)
+		printf("Failed to read kernel source.\n");
+
+	char* source_str = malloc(MAX_SOURCE_SIZE);
+	size_t source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose(fp);
+
+	struct stat st;
+	if (stat(argv[1], &st) == -1)
+		printf("Failed to stat the input file.\n");
+
+	fp = fopen(argv[1], "rb");
+	if (!fp)
+		printf("Failed to read the input file.\n");
+
+	unsigned long filesize = st.st_size;
+	unsigned char file_buf[filesize];
+
+	unsigned long buf_bytes = filesize;
+	unsigned long ints_start = filesize / sizeof(unsigned int);
+
+	fread(&file_buf, sizeof(char), filesize, fp);
+	fclose(fp);
+
+	while ((8 * ++buf_bytes) % 512 != 0);
+
+	unsigned long buf_ints = buf_bytes / sizeof(unsigned int);
+	unsigned int* int_buf = calloc(buf_ints, sizeof(unsigned int));
+
+	for (int i = 0; i < ints_start; i++) {
+		int_buf[i] =
+			((file_buf[i * sizeof(unsigned int)] << 24) & 0xFF000000) |
+			((file_buf[i * sizeof(unsigned int) + 1] << 16) & 0xFF0000) |
+			((file_buf[i * sizeof(unsigned int) + 2] << 8) & 0xFF00) |
+			(file_buf[i * sizeof(unsigned int) + 3]);
+	}
+
+	int_buf[ints_start] = 0x80000000;
+	int_buf[buf_ints - 1] = filesize * 8;
+
+	ret = clGetPlatformIDs(1, platforms, &ret_num_platforms);
+	ret = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 1, devices, &ret_num_devices);
+
+	context = clCreateContext(NULL, 1, devices, NULL, NULL, &ret);
+
+	queue = clCreateCommandQueue(context, devices[0], 0, &ret);
+
+	program = clCreateProgramWithSource(context, 1, (const char**) &source_str, (const size_t*) &source_size, &ret);
+
+	ret = clBuildProgram(program, 1, devices, NULL, NULL, NULL);
+
+	kernel = clCreateKernel(program, "sha1", &ret);
+
+	cl_mem in_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, buf_bytes, NULL, &ret);
+	cl_mem in_size = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned long), NULL, &ret);
+	cl_mem res_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned long), NULL, &ret);
+
+	ret = clEnqueueWriteBuffer(queue, in_buf, CL_TRUE, 0, buf_bytes, int_buf, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(queue, in_size, CL_TRUE, 0, sizeof(unsigned long), &buf_ints, 0, NULL, NULL);
+	ret = clEnqueueFillBuffer(queue, res_buf, (unsigned long[1]){0}, sizeof(unsigned long), 0, sizeof(unsigned long), 0, NULL, NULL);
+
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &in_buf);
+	ret = clSetKernelArg(kernel, 1, sizeof(unsigned long), &in_size);
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), &res_buf);
+
+	if (ret != CL_SUCCESS) {
+		printf("%d\n", (int) ret);
+	}
+
+	ret = clEnqueueNDRangeKernel(queue, kernel, 1, 0, (unsigned long[1]){1}, NULL, 0, NULL, NULL);
+
+	ret = clFlush(queue);
+
+	if (ret != CL_SUCCESS) {
+		printf("%d\n", (int) ret);
+	}
+
+	ret = clFinish(queue);
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseCommandQueue(queue);
+	ret = clReleaseContext(context);
+
+	free(source_str);
+	free(int_buf);
+	free(platforms);
+	free(devices);
+
+	return 0;
+}
---
+++ sha1.cl
@@ -0,0 +1,88 @@
+uint f(uint t, uint B, uint C, uint D) {
+	if (t < 20) {
+		return (B & C) | (~B & D);
+	} else if (t >= 20 && t < 40) {
+		return B ^ C ^ D;
+	} else if (t >= 40 && t < 60) {
+		return (B & C) | (B & D) | (C & D);
+	} else if (t >= 60 && t < 80) {
+		return B ^ C ^ D;
+	}
+}
+
+uint plus(uint X, uint Y) {
+	//return (X + Y) % convert_uint(pow(2.0, 32.0));
+	return (X + Y) & convert_uint(native_powr(2.0, 32.0) - 1.0);
+}
+
+uint shift(uint n, uint X) {
+	return rotate(X, n);
+}
+
+uint K(uint t) {
+	if (t < 20) {
+		return 0x5A827999;
+	} else if (t >= 20 && t < 40) {
+		return 0x6ED9EBA1;
+	} else if (t >= 40 && t < 60) {
+		return 0x8F1BBCDC;
+	} else if (t >= 60 && t < 80) {
+		return 0xCA62C1D6;
+	}
+}
+
+__kernel void sha1(__constant unsigned int* file, __constant unsigned long* filesize, __global unsigned long* res)
+{
+	uint MASK = 0x0000000F;
+	uint W[16];
+	uint H[5] = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
+	uint A, B, C, D, E;
+
+	for (int i = 0; i < (*filesize / 16); i++) {
+		for (int j = 0; j < 16; j++) {
+			  W[j] = file[16*i+j];
+		}
+
+		A = H[0];
+		B = H[1];
+		C = H[2];
+		D = H[3];
+		E = H[4];
+
+		for (uint t = 0; t < 80; t++) {
+			uint s = t & MASK;
+			uint Ws;
+			if (t >= 16) {
+				W[s] = rotate((W[(s+13) & MASK] ^ W[(s+8) & MASK] ^ W[(s+2) & MASK] ^ W[s]), (uint) 1);
+			} 
+
+			uint TEMP = plus(rotate(A, (uint) 5), f(t, B, C, D));
+			TEMP = plus(TEMP, E);
+			TEMP = plus(TEMP, W[s]);
+			TEMP = plus(TEMP, K(t));
+
+			E = D;
+			D = C;
+			C = rotate(B, (uint) 30);
+			B = A;
+			A = TEMP;
+		}
+
+		H[0] = plus(H[0], A);
+		H[1] = plus(H[1], B);
+		H[2] = plus(H[2], C);
+		H[3] = plus(H[3], D);
+		H[4] = plus(H[4], E);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		printf("%08X", H[i]);
+	}
+
+	/*for (int i = 0; i < *filesize; i++) {
+		 printf("%08X", file[i]);
+		 if (i % 8 == 0) {
+		 	//printf("\n");
+		}
+	}*/
+}
